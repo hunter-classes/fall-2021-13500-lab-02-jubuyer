710b48303f7f01f47da4de1e1b689e63d8de75b0
jubuyer <jahmed10@stuy.edu>
2021-09-08 01:03:50 -0400

    added main.cpp and funcs.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "funcs.h"
+
+int main () {
+  print_interval(1, 10);
+  std::cout << "\n";
+  print_interval(-6, 6);
+  std::cout << "\n";
+  print_interval(100,114);
+  std::cout << std::endl;
+
+  return 0;
+}
