f8cbfd9ba28c3eef2e129b667bf1976020570382
jubuyer <jahmed10@stuy.edu>
2021-09-11 22:17:37 -0400

    improved documentation in main

--- main.cpp
+++ main.cpp
@@ -16,9 +16,12 @@ L <= x < U.
 int main () {
   print_interval(1, 10);
   std::cout << "\n";
+  // should print 1 2 3 4 5 6 7 8 9
   print_interval(-6, 6);
   std::cout << "\n";
+  // should print -6 -5 -4 -3 -2 -1 0 1 2 3 4 5
   print_interval(100,114);
+  // should print 100 101 102 103 104 105 106 107 108 109 110 111 112 113
   std::cout << std::endl;
 
   return 0;
